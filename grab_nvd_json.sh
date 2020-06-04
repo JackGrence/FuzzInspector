c29ad8a6338bde2c2557f87776e724bdc3019c43
JackGrence <green1181772@gmail.com>
2020-06-04 13:26:08 +0800

    Remove and Grab new files everytime

--- grab_nvd_json.sh
+++ grab_nvd_json.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 mkdir -p ./nvdcve_json
+rm -rf ./nvdcve_json/*
 for i in `seq 2002 2020`
 do
-wget 'https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-'$i'.json.zip'
-unzip 'nvdcve_json/nvdcve-1.1-'$i'.json.zip'
+	cd nvdcve_json
+	wget 'https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-'$i'.json.zip'
+	unzip 'nvdcve-1.1-'$i'.json.zip'
+	cd ../
 done
