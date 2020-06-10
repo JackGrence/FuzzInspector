f3ec4cd1c0dcb1921a622b21a3c557b757dab41b
JackGrence <green1181772@gmail.com>
2020-06-10 19:14:33 +0800

    Add VulnBin

--- grab_function_call.py
+++ grab_function_call.py
@@ -1,6 +1,7 @@
 import r2pipe
 import argparse
 import json
+from pathlib import Path
 from asciitree import LeftAligned
 from collections import OrderedDict as OD
 
@@ -19,20 +20,41 @@ from collections import OrderedDict as OD
 #   information leak + memory corruption
 #   out of bound RW to arbitrary RW
 
-class VulnPath:
-    def __init__(self, elf, addr):
+class VulnBin:
+    def __init__(self, elf, addr_list):
         self.r2 = r2pipe.open(elf)
         self.r2.cmd('aa')
         self.r2.cmd('aac')
-        self.r2.cmd(addr)
-        self.addr = int(addr, 16)
+        self.vuln_path = {}
+        for addr in addr_list:
+            self.vuln_path[addr] = VulnPath(self.r2, int(addr, 16))
+
+    def print_block_path(self):
+        for addr, vuln_path in self.vuln_path.items():
+            print(vuln_path.block_path())
+
+    def print_call_stack(self):
+        for addr, vuln_path in self.vuln_path.items():
+            tree, symbols = vuln_path.call_stack()
+            print(tree)
+
+    def print_vuln_func(self):
+        for addr, vuln_path in self.vuln_path.items():
+            tree, symbols = vuln_path.call_stack()
+            print(symbols)
+
+    def __del__(self):
+        self.r2.quit()
+
+class VulnPath:
+    def __init__(self, elf, addr):
+        self.r2 = elf
+        self.addr = addr
+        self.r2.cmd(hex(addr))
         self.block_base = int(self.r2.cmd('ab | grep ^addr').split(' ')[1], 16)
         func_info = json.loads(self.r2.cmd('afij'))[0]
         self.func_base = func_info['offset']
         self.func_name = func_info['name']
-
-    def __del__(self):
-        self.r2.quit()
 
     def _find_callers(self, addr, callers, history):
         xrefs = json.loads(self.r2.cmd('axtj {}'.format(addr)))
@@ -51,7 +73,8 @@ class VulnPath:
             callers.add(caller['fcn_addr'])
         return OD(childs)
 
-    def _draw_call_path(self):
+    # TODO: cache
+    def call_stack(self):
         callers = {self.func_base}
         tree = {self.func_name: self._find_callers(self.func_base, callers, [])}
         tr = LeftAligned()
@@ -62,8 +85,10 @@ class VulnPath:
             symbols += symbol_list.split('\n')[:-1]
         return tr(tree), ' '.join(symbols)
 
-    def analyze(self, is_call_stack, is_block_path, is_vuln_func):
+    # TODO: cache
+    def block_path(self):
         # parse function block
+        self.r2.cmd(hex(self.addr))
         func_blocks = json.loads(self.r2.cmd('abj'))
         call_maps = {}
         for block in func_blocks:
@@ -71,19 +96,9 @@ class VulnPath:
                 call_maps.setdefault(block['jump'], []).append(('j', block['addr']))
             if 'fail' in block:
                 call_maps.setdefault(block['fail'], []).append(('f', block['addr']))
-        if is_block_path:
-            self._draw_path(call_maps, self.block_base)
-        if is_call_stack or is_vuln_func:
-            tree, callers = self._draw_call_path()
-            if is_call_stack:
-                print(tree)
-            if is_vuln_func:
-                print(callers)
-
-    def _draw_path(self, call_maps, target):
-        tree = {hex(target): self._build_tree(call_maps, target, [])}
+        tree = {hex(self.block_base): self._build_tree(call_maps, self.block_base, [])}
         tr = LeftAligned()
-        print(tr(tree))
+        return tr(tree)
 
     def _build_tree(self, call_maps, target, history):
         if target in history:
@@ -101,14 +116,25 @@ class VulnPath:
 def main():
     parser = argparse.ArgumentParser(description='find possible exploit path')
     parser.add_argument('vuln_elf', type=str)
-    parser.add_argument('vuln_addr', type=str)
+    parser.add_argument('vuln_addr', nargs='*', type=str)
     parser.add_argument('-c', '--call-stack', help='show call stack', action='store_true')
     parser.add_argument('-b', '--block-path', help='show block path', action='store_true')
     parser.add_argument('-f', '--vuln-functions', help='list vulnerable functions', action='store_true')
+    parser.add_argument('-r', '--rootfs', default='./', help='find other vulnerable binaries', type=str)
+    parser.add_argument('-t', '--targets', default=[], nargs='*', type=str, help='interested binaries for exploit entry')
+    parser.add_argument('-e', '--exploit-path', help='show exploit path from targets', action='store_true')
     
     args = parser.parse_args()
-    vuln_path = VulnPath(args.vuln_elf, args.vuln_addr)
-    vuln_path.analyze(args.call_stack, args.block_path, args.vuln_functions)
+    vuln_lib = list(Path(args.rootfs).rglob(args.vuln_elf))[0].absolute()
+    vuln_lib = str(vuln_lib)
+    binaries = {}
+    binaries[vuln_lib] = VulnBin(vuln_lib, args.vuln_addr)
+    if args.block_path:
+        binaries[vuln_lib].print_block_path()
+    if args.call_stack:
+        binaries[vuln_lib].print_call_stack()
+    if args.vuln_functions:
+        binaries[vuln_lib].print_vuln_func()
 
 if __name__ == '__main__':
     main()
