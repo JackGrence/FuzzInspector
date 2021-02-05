422b1149e03ed2185a84e6b687a7b3fa0094947a
JackGrence <green1181772@gmail.com>
2021-02-05 17:45:28 +0800

    Add memory limit

--- fuzz.sh
+++ fuzz.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-AFL_AUTORESUME=1 AFL_PATH="$(realpath ~/code/AFLplusplus/)" PATH="$AFL_PATH:$PATH" afl-fuzz -i afl_inputs -o afl_outputs -U -L 0 -Z -- python ./ql.py @@
+AFL_AUTORESUME=1 AFL_PATH="$(realpath ~/code/AFLplusplus/)" PATH="$AFL_PATH:$PATH" afl-fuzz -m 300 -i afl_inputs -o afl_outputs -U -L 0 -Z -- python ./ql.py @@
