030b895674b60859a0aa0b2c361bed638eb94875
JackGrence <green1181772@gmail.com>
2021-03-01 22:40:37 +0800

    Remove old layout

--- static/tree.js
+++ static/tree.js
@@ -2,10 +2,14 @@ function execute() {
   let address = $("#dropdownDisasToggle").text().split(":")[0];
   let context = $("#inputCtx").val();
   if ($("#nav-tab").attr("select") == "CPUState") {
+    // display loading block
     $("#nav-cpustate .context-loading").removeClass("d-none");
+    // call CPUState API
     $.getJSON("/cpustate", {"address": address, "context": context});
   } else {
+    // display loading block
     $("#nav-relation .context-loading").removeClass("d-none");
+    // call relationship API
     $.getJSON("/relationship", {"address": address, "context": context});
     console.log("relationship");
   }
@@ -36,14 +40,6 @@ function showBitmap(data, nodes, network) {
   }
 }
 
-function prepareRelationship() {
-  loading = '<div class="spinner-border" role="status"><span class="sr-only">Loading...</span></div>';
-  $("#relationship").html($("#relationship").html() + loading);
-  addr = $("#relationAddr").val();
-  ctx = $("#relationCtx").val();
-  $.getJSON("/relationship", {"address": addr, "context": ctx});
-}
-
 function showRelationship(data) {
   if (data !== '') {
     $("#nav-relation .context-loading").addClass("d-none");
@@ -51,206 +47,10 @@ function showRelationship(data) {
   }
 }
 
-function prepareCPUState() {
-  address = this.textContent;
-  $("#relationAddr").val(address);
-  $("#loading").html('<div class="spinner-border" role="status"><span class="sr-only">Loading...</span></div>');
-  $.getJSON("/cpustate", {"address": address, "context": $("#userCtx").val()});
-}
-
 function showCPUState(data) {
   if (data !== '') {
     $("#nav-cpustate .context-loading").addClass("d-none");
     $("#divCPUState").html(data);
-  }
-}
-
-function showAssembly(e) {
-  address = e.data.name;
-	/*
-<div class="input-group mb-3">
-  <div class="input-group-prepend">
-    <button class="btn btn-outline-secondary" type="button">Button</button>
-  </div>
-  <input type="text" class="form-control" placeholder="" aria-label="" aria-describedby="basic-addon1">
-</div>
-	 */
-  $.getJSON("/basicblock/disassemble", {"address": address}).done(function(data, status){
-    $("#disasm").html('');
-    $(data).each(function() {
-      var group = $("<div>").prop("class", "input-group");
-      var addrBtn = $("<div>").prop("class", "input-group-prepend")
-		    .append($("<button>").prop("class", "btn btn-outline-secondary")
-		    .prop("type", "button").text("0x" + this["offset"].toString(16)))
-		    .click(prepareCPUState);
-      var asmText = $("<input>").prop("type", "text").prop("class", "form-control")
-		    .prop("aria-describedby", "basic-addon1")
-		    .prop("value", this["opcode"]);
-      $("#disasm").append(group.append(addrBtn).append(asmText));
-    });
-  });
-}
-
-function redraw() {
-  // Assigns parent, children, height, depth
-  root = d3.hierarchy(treeData, function(d) { return d.children; });
-  root.x0 = height / 2;
-  root.y0 = 0;
-
-  // Collapse after the second level
-  // root.children.forEach(collapse);
-
-  update(root);
-}
-
-// Collapse the node and all it's children
-function collapse(d) {
-  if(d.children) {
-    d._children = d.children
-    d._children.forEach(collapse)
-    d.children = null
-  }
-}
-
-function update(source) {
-
-  // Assigns the x and y position for the nodes
-  var treeData = treemap(root);
-
-  // Compute the new tree layout.
-  var nodes = treeData.descendants(),
-      links = treeData.descendants().slice(1);
-
-  // Normalize for fixed-depth.
-  nodes.forEach(function(d){ d.y = d.depth * 180});
-
-  // ****************** Nodes section ***************************
-
-  // Update the nodes...
-  var node = svg.selectAll('g.node')
-      .data(nodes, function(d) {return d.id || (d.id = ++i); });
-
-  // Enter any new modes at the parent's previous position.
-  var nodeEnter = node.enter().append('g')
-      .attr('class', 'node')
-      .attr("transform", function(d) {
-        return "translate(" + source.y0 + "," + source.x0 + ")";
-    })
-    .on('click', showAssembly);
-
-  // Add Circle for the nodes
-  nodeEnter.append('circle')
-      .attr('class', 'node')
-      .attr('r', 1e-6)
-      .style("fill", function(d) {
-          return d._children ? "lightsteelblue" : "#fff";
-      });
-
-  // Add labels for the nodes
-  nodeEnter.append('text')
-      .attr("id", function(d) { return d.data.name; })
-      .attr("dy", ".35em")
-      .attr("x", function(d) {
-          return d.children || d._children ? -13 : 13;
-      })
-      .attr("text-anchor", function(d) {
-          return d.children || d._children ? "end" : "start";
-      })
-      .text(function(d) { return d.data.name; });
-
-  // UPDATE
-  var nodeUpdate = nodeEnter.merge(node);
-
-  // Transition to the proper position for the node
-  nodeUpdate.transition()
-    .duration(duration)
-    .attr("transform", function(d) {
-        return "translate(" + d.y + "," + d.x + ")";
-     });
-
-  // Update the node attributes and style
-  nodeUpdate.select('circle.node')
-    .attr('r', 10)
-    .style("fill", function(d) {
-        return d._children ? "lightsteelblue" : "#fff";
-    })
-    .attr('cursor', 'pointer');
-
-
-  // Remove any exiting nodes
-  var nodeExit = node.exit().transition()
-      .duration(duration)
-      .attr("transform", function(d) {
-          return "translate(" + source.y + "," + source.x + ")";
-      })
-      .remove();
-
-  // On exit reduce the node circles size to 0
-  nodeExit.select('circle')
-    .attr('r', 1e-6);
-
-  // On exit reduce the opacity of text labels
-  nodeExit.select('text')
-    .style('fill-opacity', 1e-6);
-
-  // ****************** links section ***************************
-
-  // Update the links...
-  var link = svg.selectAll('path.link')
-      .data(links, function(d) { return d.id; });
-
-  // Enter any new links at the parent's previous position.
-  var linkEnter = link.enter().insert('path', "g")
-      .attr("class", "link")
-      .attr('d', function(d){
-        var o = {x: source.x0, y: source.y0}
-        return diagonal(o, o)
-      });
-
-  // UPDATE
-  var linkUpdate = linkEnter.merge(link);
-
-  // Transition back to the parent element position
-  linkUpdate.transition()
-      .duration(duration)
-      .attr('d', function(d){ return diagonal(d, d.parent) });
-
-  // Remove any exiting links
-  var linkExit = link.exit().transition()
-      .duration(duration)
-      .attr('d', function(d) {
-        var o = {x: source.x, y: source.y}
-        return diagonal(o, o)
-      })
-      .remove();
-
-  // Store the old positions for transition.
-  nodes.forEach(function(d){
-    d.x0 = d.x;
-    d.y0 = d.y;
-  });
-
-  // Creates a curved (diagonal) path from parent to the child nodes
-  function diagonal(s, d) {
-
-    path = `M ${s.y} ${s.x}
-            C ${(s.y + d.y) / 2} ${s.x},
-              ${(s.y + d.y) / 2} ${d.x},
-              ${d.y} ${d.x}`
-
-    return path
-  }
-
-  // Toggle children on click.
-  function click(d) {
-    if (d.children) {
-        d._children = d.children;
-        d.children = null;
-      } else {
-        d.children = d._children;
-        d._children = null;
-      }
-    update(d);
   }
 }
 
