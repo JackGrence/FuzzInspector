97674421fb580fafdcbec64051d290b46cb39294
JackGrence <green1181772@gmail.com>
2021-03-22 11:34:43 +0800

    Remove multiple flask run

--- visrun.sh
+++ visrun.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-FLASK_ENV=development FLASK_APP=httpserver.py flask run FLASK_RUN_PORT=`cat visport` flask run
+FLASK_ENV=development FLASK_APP=httpserver.py FLASK_RUN_PORT=`cat visport` flask run
